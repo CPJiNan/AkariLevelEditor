849222ca2353258066f10f93c175739d2a11a196
季楠 <2759278070@qq.com>
2025-03-06 19:31:00 +0800

    [+]新增若干内容

--- Pages/HomePage.xaml.cs
+++ Pages/HomePage.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Windows;
-using Wpf.Ui.Controls;
 
 namespace AkariLevelEditor.Pages;
 
@@ -13,8 +11,8 @@ public partial class HomePage : INotifyPropertyChanged
     private bool _checkUpdate = true;
     private bool _opNotify = true;
     private bool _sendMetrics = true;
-    private bool _debug = false;
-    private bool _useUUID = false;
+    private bool _debug;
+    private bool _useUuid;
     private string _selectedDatabaseMethod = "JSON";
     private string _jsonFilePath = "database.json";
     private string _sqlHost = "localhost";
@@ -23,16 +21,16 @@ public partial class HomePage : INotifyPropertyChanged
     private string _sqlPassword = "password";
     private string _sqlDatabase = "minecraft";
     private string _sqlTable = "akarilevel";
-    private bool _vanillaTrace = false;
+    private bool _vanillaTrace;
     private string _defaultTraceGroup = "";
     private bool _autoResetTrace = true;
-    private bool _teamEnable = false;
+    private bool _teamEnable;
     private string _selectedTeamPlugin = "DungeonPlus";
     private string _teamSource = "MYTHICMOBS_DROP_EXP";
     private string _teamFormula = "%exp% * %size%";
     private double _teamLeaderWeight = 1;
     private double _teamMemberWeight = 1;
-    private bool _attributeEnable = false;
+    private bool _attributeEnable;
     private string _selectedAttributePlugin = "AttributePlus";
     private string _attributeName = "经验加成";
     private string _attributeFormula = "%exp% * ( 1 + %attribute% / 100 )";
@@ -45,10 +43,12 @@ public partial class HomePage : INotifyPropertyChanged
     private string _expBarFull = "■";
     private int _expBarLength = 10;
 
-    public ObservableCollection<string> Languages { get; } = new() { "zh_CN", "zh_TW", "en_US", "es_ES" };
-    public ObservableCollection<string> DatabaseMethods { get; } = new() { "JSON", "SQL" };
-    public ObservableCollection<string> TeamPlugins { get; } = new() { "DungeonPlus" };
-    public ObservableCollection<string> AttributePlugins { get; } = new() { "AttributePlus", "SX-Attribute", "OriginAttribute", "AttributeSystem" };
+    public ObservableCollection<string> Languages { get; } = ["zh_CN", "zh_TW", "en_US", "es_ES"];
+    public ObservableCollection<string> DatabaseMethods { get; } = ["JSON", "SQL"];
+    public ObservableCollection<string> TeamPlugins { get; } = ["DungeonPlus"];
+
+    public ObservableCollection<string> AttributePlugins { get; } =
+        ["AttributePlus", "SX-Attribute", "OriginAttribute", "AttributeSystem"];
 
     public string SelectedLanguage
     {
@@ -116,12 +116,12 @@ public partial class HomePage : INotifyPropertyChanged
         }
     }
 
-    public bool UseUUID
+    public bool UseUuid
     {
-        get => _useUUID;
+        get => _useUuid;
         set
         {
-            _useUUID = value;
+            _useUuid = value;
             OnPropertyChanged();
             UpdatePreview();
         }
@@ -446,11 +446,9 @@ public partial class HomePage : INotifyPropertyChanged
         }
     }
 
-    public bool IsJsonSelected => SelectedDatabaseMethod == "JSON";
-    public bool IsSqlSelected => SelectedDatabaseMethod == "SQL";
+    private string? _previewText;
 
-    private string _previewText;
-    public string PreviewText
+    public string? PreviewText
     {
         get => _previewText;
         set
@@ -469,141 +467,143 @@ public partial class HomePage : INotifyPropertyChanged
 
     private void UpdatePreview()
     {
-        PreviewText = $@"#     _    _              _ _                   _  #
-#    / \  | | ____ _ _ __(_) |    _____   _____| | #
-#   / _ \ | |/ / _` | '__| | |   / _ \ \ / / _ \ | #
-#  / ___ \|   < (_| | |  | | |__|  __/\ V /  __/ | #
-# /_/   \_\_|\_\__,_|_|  |_|_____\___| \_/ \___|_| #
+        PreviewText = $"""
+                       #     _    _              _ _                   _  #
+                       #    / \  | | ____ _ _ __(_) |    _____   _____| | #
+                       #   / _ \ | |/ / _` | '__| | |   / _ \ \ / / _ \ | #
+                       #  / ___ \|   < (_| | |  | | |__|  __/\ V /  __/ | #
+                       # /_/   \_\_|\_\__,_|_|  |_|_____\___| \_/ \___|_| #
 
-# 插件文档: https://cpjinan.github.io/Wiki/
+                       # 插件文档: https://cpjinan.github.io/Wiki/
 
-##################################
-#       AkariLevel  Config       #
-##################################
+                       ##################################
+                       #       AkariLevel  Config       #
+                       ##################################
 
-###########
-# Options #
-###########
+                       ###########
+                       # Options #
+                       ###########
 
-# 全局设置
-Options:
-  # 语言
-  Language: ""{SelectedLanguage}""
-  # 配置文件版本
-  Config-Version: {ConfigVersion}
-  # 检查版本更新
-  Check-Update: {CheckUpdate.ToString().ToLower()}
-  # OP 版本更新通知
-  OP-Notify: {OpNotify.ToString().ToLower()}
-  # 启用 bStats 统计
-  Send-Metrics: {SendMetrics.ToString().ToLower()}
-  # 启用调试模式
-  Debug: {Debug.ToString().ToLower()}
+                       # 全局设置
+                       Options:
+                         # 语言
+                         Language: "{SelectedLanguage}"
+                         # 配置文件版本
+                         Config-Version: {ConfigVersion}
+                         # 检查版本更新
+                         Check-Update: {CheckUpdate.ToString().ToLower()}
+                         # OP 版本更新通知
+                         OP-Notify: {OpNotify.ToString().ToLower()}
+                         # 启用 bStats 统计
+                         Send-Metrics: {SendMetrics.ToString().ToLower()}
+                         # 启用调试模式
+                         Debug: {Debug.ToString().ToLower()}
 
-############
-# Database #
-############
+                       ############
+                       # Database #
+                       ############
 
-# 数据存储设置
-Database:
-  # 使用玩家 UUID 进行存储
-  UUID: {UseUUID.ToString().ToLower()}
-  # 存储方式 (JSON, SQL)
-  Method: {SelectedDatabaseMethod}
-  JSON:
-    file: {JsonFilePath}
-  SQL:
-    host: {SqlHost}
-    port: {SqlPort}
-    user: {SqlUser}
-    password: {SqlPassword}
-    database: {SqlDatabase}
-    table: {SqlTable}
+                       # 数据存储设置
+                       Database:
+                         # 使用玩家 UUID 进行存储
+                         UUID: {UseUuid.ToString().ToLower()}
+                         # 存储方式 (JSON, SQL)
+                         Method: {SelectedDatabaseMethod}
+                         JSON:
+                           file: {JsonFilePath}
+                         SQL:
+                           host: {SqlHost}
+                           port: {SqlPort}
+                           user: {SqlUser}
+                           password: {SqlPassword}
+                           database: {SqlDatabase}
+                           table: {SqlTable}
 
-#########
-# Trace #
-#########
+                       #########
+                       # Trace #
+                       #########
 
-# 等级组追踪设置
-Trace:
-  # 是否保留原版等级系统
-  # 该功能开启后将不会取消原版经验变化事件，但需注意关闭等级组的追踪功能
-  Vanilla: {VanillaTrace.ToString().ToLower()}
-  # 失去追踪焦点时返回的默认等级组 (此项留空则不会进行追踪)
-  Default: ""{DefaultTraceGroup}""
-  # 是否在玩家每次进入服务器时重置追踪的等级组
-  Auto-Reset: {AutoResetTrace.ToString().ToLower()}
+                       # 等级组追踪设置
+                       Trace:
+                         # 是否保留原版等级系统
+                         # 该功能开启后将不会取消原版经验变化事件，但需注意关闭等级组的追踪功能
+                         Vanilla: {VanillaTrace.ToString().ToLower()}
+                         # 失去追踪焦点时返回的默认等级组 (此项留空则不会进行追踪)
+                         Default: "{DefaultTraceGroup}"
+                         # 是否在玩家每次进入服务器时重置追踪的等级组
+                         Auto-Reset: {AutoResetTrace.ToString().ToLower()}
 
-########
-# Team #
-########
+                       ########
+                       # Team #
+                       ########
 
-# 队伍经验共享设置
-Team:
-  # 是否启用
-  Enable: {TeamEnable.ToString().ToLower()}
-  # 组队插件 (DungeonPlus)
-  Plugin: {SelectedTeamPlugin}
-  # 参与共享的经验来源
-  Source:
-    - ""{TeamSource}""
-  # 待分配的经验池总额 (JavaScript 支持)
-  # %exp% -> 分配前的经验数额
-  # %size% -> 队伍人数
-  Total: ""{TeamFormula}""
-  # 队长与队员各自的分配权重，实际所获经验 = 经验池总额 * ( 个人权重 / 队伍权重总和 )
-  Weight:
-    Leader: {TeamLeaderWeight}
-    Member: {TeamMemberWeight}
+                       # 队伍经验共享设置
+                       Team:
+                         # 是否启用
+                         Enable: {TeamEnable.ToString().ToLower()}
+                         # 组队插件 (DungeonPlus)
+                         Plugin: {SelectedTeamPlugin}
+                         # 参与共享的经验来源
+                         Source:
+                           - "{TeamSource}"
+                         # 待分配的经验池总额 (JavaScript 支持)
+                         # %exp% -> 分配前的经验数额
+                         # %size% -> 队伍人数
+                         Total: "{TeamFormula}"
+                         # 队长与队员各自的分配权重，实际所获经验 = 经验池总额 * ( 个人权重 / 队伍权重总和 )
+                         Weight:
+                           Leader: {TeamLeaderWeight}
+                           Member: {TeamMemberWeight}
 
-#############
-# Attribute #
-#############
+                       #############
+                       # Attribute #
+                       #############
 
-# 属性插件经验加成
-Attribute:
-  # 是否启用
-  Enable: {AttributeEnable.ToString().ToLower()}
-  # 属性插件 (AttributePlus, SX-Attribute, OriginAttribute, AttributeSystem)
-  Plugin: ""{SelectedAttributePlugin}""
-  # 属性名称 (如属性插件为 OriginAttribute 则无需填写此项)
-  # AttributePlus - ""经验加成""
-  # SX-Attribute - ""ExpAddition""
-  # AttributeSystem - ""ExpAddition""
-  Name: ""{AttributeName}""
-  # 经验计算公式 (JavaScript支持)
-  # %exp% -> 获得的经验值数量
-  # %attribute% -> 额外加成经验数量
-  Formula: ""{AttributeFormula}""
-  # 所监听的 PlayerExpChangeEvent 来源
-  Source:
-    - ""{AttributeSource}""
+                       # 属性插件经验加成
+                       Attribute:
+                         # 是否启用
+                         Enable: {AttributeEnable.ToString().ToLower()}
+                         # 属性插件 (AttributePlus, SX-Attribute, OriginAttribute, AttributeSystem)
+                         Plugin: "{SelectedAttributePlugin}"
+                         # 属性名称 (如属性插件为 OriginAttribute 则无需填写此项)
+                         # AttributePlus - "经验加成"
+                         # SX-Attribute - "ExpAddition"
+                         # AttributeSystem - "ExpAddition"
+                         Name: "{AttributeName}"
+                         # 经验计算公式 (JavaScript支持)
+                         # %exp% -> 获得的经验值数量
+                         # %attribute% -> 额外加成经验数量
+                         Formula: "{AttributeFormula}"
+                         # 所监听的 PlayerExpChangeEvent 来源
+                         Source:
+                           - "{AttributeSource}"
 
-####################
-#  PlaceholderAPI  #
-####################
+                       ####################
+                       #  PlaceholderAPI  #
+                       ####################
 
-# 变量设置
-PlaceholderAPI:
-  # 变量前缀
-  Identifier: ""{PlaceholderPrefix}""
-  # 经验条变量设置
-  Progress-Bar:
-    # 当前等级 / 最高等级
-    Level:
-      Empty: ""{LevelBarEmpty}""
-      Full: ""{LevelBarFull}""
-      Length: {LevelBarLength}
-    # 当前经验 / 升至下一级所需经验
-    Exp:
-      Empty: ""{ExpBarEmpty}""
-      Full: ""{ExpBarFull}""
-      Length: {ExpBarLength}";
+                       # 变量设置
+                       PlaceholderAPI:
+                         # 变量前缀
+                         Identifier: "{PlaceholderPrefix}"
+                         # 经验条变量设置
+                         Progress-Bar:
+                           # 当前等级 / 最高等级
+                           Level:
+                             Empty: "{LevelBarEmpty}"
+                             Full: "{LevelBarFull}"
+                             Length: {LevelBarLength}
+                           # 当前经验 / 升至下一级所需经验
+                           Exp:
+                             Empty: "{ExpBarEmpty}"
+                             Full: "{ExpBarFull}"
+                             Length: {ExpBarLength}
+                       """;
     }
 
-    public event PropertyChangedEventHandler PropertyChanged;
+    public event PropertyChangedEventHandler? PropertyChanged;
 
-    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
